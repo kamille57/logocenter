b2533c4b383991b5632796ffa54a394cf143a530
kamille57 <126716411+kamille57@users.noreply.github.com>
2023-10-06 00:40:04 +0300

    why section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import Header from "./Header.jsx";
 import Courses from "./Courses.jsx";
 import Footer from "./Footer.jsx";
 import Whome from "./Whome.jsx";
+import Why from "./Why.jsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Courses />
       <Whome />
+      <Why />
       <Footer />
     </div>
   );
