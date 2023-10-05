ebe522c2ebca9500ae9600eede65dcb4173562b8
Dmitry <mityourik@gmail.com>
2023-10-05 03:22:44 -0300

    add section courses

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "./Header.jsx";
 import Main from "./Main.jsx";
+import Courses from "./Courses.jsx";
 
 function App() {
   return (
     <div className="page">
       <Header />
+      <Courses />
     </div>
   );
 }
---
+++ src/components/Courses.jsx
@@ -0,0 +1,77 @@
+
+function Courses () {
+    return (
+        <section className="courses">
+            <div className="courses__container">
+                <div className="courses__title-group">
+                    <h1 className="courses__title">Направления речевой терапии</h1>
+                    <button className="courses__button">Все курсы??</button>
+                </div>
+                <div className="courses__grid">
+                    <article className="courses__card-item">
+                        <div className="card-item__container">
+                            <img className="card-item__img"></img>
+                            <h2 className="card-item__title">Запуск речи</h2>
+                            <p className="card-item__subtitle">Длительность:<span className="card-item__span">45 минут</span></p>
+                            <p className="card-item__subtitle">Этапов:<span className="card-item__span">3 занятия</span></p>
+                            <a className="card-item__link">Узнать подробнее<img className="card-item__img-arrow" /></a>
+                            <p className="card-item__price">От 1875р.</p>
+                        </div>
+                    </article>
+                    <article className="courses__card-item">
+                        <div className="card-item__container">
+                            <img className="card-item__img"></img>
+                            <h2 className="card-item__title">Запуск речи</h2>
+                            <p className="card-item__subtitle">Длительность:<span className="card-item__span">45 минут</span></p>
+                            <p className="card-item__subtitle">Этапов:<span className="card-item__span">3 занятия</span></p>
+                            <a className="card-item__link">Узнать подробнее<img className="card-item__img-arrow" /></a>
+                            <p className="card-item__price">От 1875р.</p>
+                        </div>
+                    </article>
+                    <article className="courses__card-item">
+                        <div className="card-item__container">
+                            <img className="card-item__img"></img>
+                            <h2 className="card-item__title">Запуск речи</h2>
+                            <p className="card-item__subtitle">Длительность:<span className="card-item__span">45 минут</span></p>
+                            <p className="card-item__subtitle">Этапов:<span className="card-item__span">3 занятия</span></p>
+                            <a className="card-item__link">Узнать подробнее<img className="card-item__img-arrow" /></a>
+                            <p className="card-item__price">От 1875р.</p>
+                        </div>
+                    </article>
+                    <article className="courses__card-item">
+                        <div className="card-item__container">
+                            <img className="card-item__img"></img>
+                            <h2 className="card-item__title">Запуск речи</h2>
+                            <p className="card-item__subtitle">Длительность:<span className="card-item__span">45 минут</span></p>
+                            <p className="card-item__subtitle">Этапов:<span className="card-item__span">3 занятия</span></p>
+                            <a className="card-item__link">Узнать подробнее<img className="card-item__img-arrow" /></a>
+                            <p className="card-item__price">От 1875р.</p>
+                        </div>
+                    </article>
+                    <article className="courses__card-item">
+                        <div className="card-item__container">
+                            <img className="card-item__img"></img>
+                            <h2 className="card-item__title">Запуск речи</h2>
+                            <p className="card-item__subtitle">Длительность:<span className="card-item__span">45 минут</span></p>
+                            <p className="card-item__subtitle">Этапов:<span className="card-item__span">3 занятия</span></p>
+                            <a className="card-item__link">Узнать подробнее<img className="card-item__img-arrow" /></a>
+                            <p className="card-item__price">От 1875р.</p>
+                        </div>
+                    </article>
+                    <article className="courses__card-item">
+                        <div className="card-item__container">
+                            <img className="card-item__img"></img>
+                            <h2 className="card-item__title">Запуск речи</h2>
+                            <p className="card-item__subtitle">Длительность:<span className="card-item__span">45 минут</span></p>
+                            <p className="card-item__subtitle">Этапов:<span className="card-item__span">3 занятия</span></p>
+                            <a className="card-item__link">Узнать подробнее<img className="card-item__img-arrow" /></a>
+                            <p className="card-item__price">От 1875р.</p>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Courses;
